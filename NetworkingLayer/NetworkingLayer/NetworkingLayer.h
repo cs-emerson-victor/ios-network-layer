abb768857d585e501351728185a2c05eb342e246
Emerson Victor <emerson.victor.f.luz@concrete.com.br>
2020-04-17 15:06:55 -0300

    Create Xcode project

---
+++ NetworkingLayer/NetworkingLayer/NetworkingLayer.h
@@ -0,0 +1,19 @@
+//
+//  NetworkingLayer.h
+//  NetworkingLayer
+//
+//  Created by emerson.victor.f.luz on 17/04/20.
+//  Copyright © 2020 Emer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkingLayer.
+FOUNDATION_EXPORT double NetworkingLayerVersionNumber;
+
+//! Project version string for NetworkingLayer.
+FOUNDATION_EXPORT const unsigned char NetworkingLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkingLayer/PublicHeader.h>
+
+
